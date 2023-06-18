fb10d7818f9a193b7e28dc5a7d5535dff54c4a12
Aditya Tiwari <tiwariaditya973@gmail.com>
2023-06-18 15:00:02 +0530

    setup of smtp using nodemailer done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const {sendBasicEmail} = require('./services/email-service');
 dotenv.config();
 
 const PORT = process.env.PORT || 3003;
@@ -12,6 +13,12 @@ const setUpAndStartServer = () => {
     app.listen(PORT,()=> {
         console.log('server listening to ',PORT);
 
+        sendBasicEmail(
+            'tiwariaditya973@gmail.com',
+            'tiwaririshab9261@gmail.com',
+            'this is a testing email',
+            'Hi how high are you hope you like the support'
+        );
     });
 }
 
