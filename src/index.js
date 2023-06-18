08282911b0ff4b12aaf0da6f67779811656bd659
Aditya Tiwari <tiwariaditya973@gmail.com>
2023-06-18 15:21:03 +0530

    added cron scheduling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const {sendBasicEmail} = require('./services/email-service');
+const cron = require('node-cron');
 dotenv.config();
 
 const PORT = process.env.PORT || 3003;
@@ -19,6 +20,9 @@ const setUpAndStartServer = () => {
             'this is a testing email',
             'Hi how high are you hope you like the support'
         );
+        cron.schedule('* * * * *',()=>{
+            console.log('running a task every minute')
+        })
     });
 }
 
